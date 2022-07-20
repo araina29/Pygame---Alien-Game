16aebbd7352f3d3416179709d0f320e29c8c69b5
araina25 <araina1@ualberta.ca>
2022-07-21 01:53:54 +0530

    Made some minor changes

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Space Invaders")
 
 #Adding an image on the screen 
-image = pygame.image.load("image.jpg")
+image = pygame.image.load('image.png')
 #X and Y coordinates for player 1 
 x_1 = 400 
 y_1 = 550
 
 #showing player one on the screen
 def player():
-    pygame.blit(image,(x_1,y_1))
+    screen.blit(image,(x_1,y_1))
 
 
 #The game loop and used for closing the window
@@ -28,7 +28,7 @@ while program_run:
         if event.type == pygame.QUIT:
             program_run = False
     #RGB -- Red, Green ,BLue
-    screen.fill((255,0,0))
+    screen.fill((0,0,0))
     player()
     pygame.display.update()
 
